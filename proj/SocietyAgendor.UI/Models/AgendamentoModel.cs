dcdc9d6bd77fdd44a72cf44d0ea39c2a092a25de
LEONARDO ALVES <leonardo.alves17@fatec.sp.gov.br>
2019-03-29 16:34:32 -0300

    Parte feita do agendamento

---
+++ proj/SocietyAgendor.UI/Models/AgendamentoModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocietyAgendor.UI.Models
+{
+    public class AgendamentoModel
+    {
+        public int? Agendamento_Id { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "É permitido até 200 caracteres.")]
+        public string Agendamento_Descricao { get; set; }
+
+        public DateTime DataAgendamento { get; set; }
+
+        public int DiaSemana_Id { get; set; }
+
+        public string DiaSemana_Desc { get; set; }
+
+        public int Horario_Id { get; set; }
+
+        public string Horario_Desc { get; set; }
+
+        public int Cliente_Id { get; set; }
+
+        public string Cliente_Nome { get; set; }
+
+        public int Estabelecimento_Id { get; set; }
+
+        public string Estabelecimento_Nome { get; set; }
+    }
+}
---
+++ proj/SocietyAgendor.UI/Models/HorarioDisponivelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocietyAgendor.UI.Models
+{
+    public class HorarioDisponivelModel
+    {
+        public int Horario_Id { get; set; }
+
+        public string Horario_Desc { get; set; }
+    }
+}
