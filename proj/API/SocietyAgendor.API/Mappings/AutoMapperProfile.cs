64ee8ed526979cba4fc4da7383da4904b8d7633b
viavn <vi.avn@hotmail.com>
2019-05-21 21:49:20 -0300

    feature: added automapper to cliente

--- proj/API/SocietyAgendor.API/Mappings/AutoMapperProfile.cs
+++ proj/API/SocietyAgendor.API/Mappings/AutoMapperProfile.cs
@@ -14,6 +14,7 @@ namespace SocietyAgendor.API.Mappings
             CreateMap<Cargo, CargoModel>().ReverseMap();
             CreateMap<Horario, HorarioModel>().ReverseMap();
             CreateMap<HorarioDisponivel, HorarioDisponivelModel>().ReverseMap();
+            CreateMap<Cliente, ClienteModel>().ReverseMap();
         }
     }
 }
