340793060ab793c00917b93c9432feb8cd9f4b5e
viavn <vi.avn@hotmail.com>
2019-05-30 23:49:53 -0300

    percorrendo com for

--- proj/SocietyAgendor.UI/Controllers/ExportExcelController.cs
+++ proj/SocietyAgendor.UI/Controllers/ExportExcelController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,160 @@ namespace SocietyAgendor.UI.Controllers
             return View();
         }
 
+        private ICell CreateCell(IRow row, int cellIndex, string cellValue, ICellStyle style)
+        {
+            ICell cell = row.CreateCell(cellIndex);
+            cell.SetCellValue(cellValue);
+            cell.SetCellType(CellType.String);
+            cell.CellStyle = style;
+
+            return cell;
+        }
+
+        private ICell CreateCell(IRow row, int cellIndex, double cellValue, ICellStyle style)
+        {
+            ICell cell = row.CreateCell(cellIndex);
+            cell.SetCellValue(cellValue);
+            cell.SetCellType(CellType.Numeric);
+            cell.CellStyle = style;
+
+            return cell;
+        }
+
+        private void CreateDiaCell(int i, int j, int k, IRow row, ICellStyle cellStyle)
+        {
+            if (i == 0)
+            {
+                if (j == 0 && k == 0)
+                {
+                    CreateCell(row, k, "DIA", cellStyle);
+                }
+                else if ((j == 1 && k == 0) || (j == 2 && k == 0))
+                {
+                    CreateCell(row, k, "", cellStyle);
+                }
+            }
+        }
+
+        private void CreateHoraCell(int i, int j, int k, IRow row, ICellStyle cellStyle)
+        {
+            if (i == 0)
+            {
+                if (j == 0 && k == 1)
+                {
+                    CreateCell(row, k, "HORA", cellStyle);
+                }
+                else if ((j == 1 && k == 1) || (j == 2 && k == 1))
+                {
+                    CreateCell(row, k, "", cellStyle);
+                }
+            }
+        }
+
+        private void CreateContagemCell(int qtdSentido, int i, int j, int k, IRow row, ICellStyle cellStyle)
+        {
+            if (i == 0)
+            {
+                if (j == 0 && k == 2)
+                    CreateCell(row, k, "CONTAGEM", cellStyle);
+
+                if (qtdSentido == 1)
+                {
+                    if (j == 0 && k == 3)
+                        CreateCell(row, k, "", cellStyle);
+                }
+                else
+                {
+                    if (j == 0 && k >= 3 && k <= 5)
+                        CreateCell(row, k, "", cellStyle);
+                }
+            }
+        }
+
+        private void CreateVelocidadeSentidoCell(int qtdSentido, int i, int j, int k, IRow row, ICellStyle cellStyle)
+        {
+            if (i == 0)
+            {
+                if (qtdSentido > 1)
+                {
+                    if (j == 0 && k == 6)
+                    {
+                        CreateCell(row, k, "VELOCIDADE SENTIDO", cellStyle);
+                    }
+                    else if (j == 0 && k >= 7 && k <= 11)
+                    {
+                        CreateCell(row, k, "", cellStyle);
+                    }
+                }
+                else
+                {
+                    if (j == 0 && k == 4)
+                    {
+                        CreateCell(row, k, "VELOCIDADE SENTIDO", cellStyle);
+                    }
+                    else if (j == 0 && k >= 5 && k <= 6)
+                    {
+                        CreateCell(row, k, "", cellStyle);
+                    }
+                }
+            }
+        }
+
+        private void CreateSentidosCell(Dictionary<int, string> sentidos, int i, int j, int k, IRow row, ICellStyle cellStyle)
+        {
+            string sentidoDesc;
+
+            // se for o primeiro sentido
+            if (i == 0)
+            {
+                sentidoDesc = sentidos.FirstOrDefault(x => x.Key == (i + 1)).Value;
+
+                if (j == 1)
+                {
+                    if (k == 2)
+                        CreateCell(row, k, sentidoDesc, cellStyle);
+                    else if (k == 3)
+                        CreateCell(row, k, "", cellStyle);
+
+                    if (sentidos.Count > 1)
+                    {
+                        if (k == 6)
+                            CreateCell(row, k, sentidoDesc, cellStyle);
+                        else if (k >= 7 && k <= 8)
+                            CreateCell(row, k, "", cellStyle);
+                    }
+                    else
+                    {
+                        if (k == 4)
+                        {
+                            CreateCell(row, k, sentidoDesc, cellStyle);
+                        }
+                        else if (k >= 5 && k <= 6)
+                        {
+                            CreateCell(row, k, "", cellStyle);
+                        }
+                    }
+                }
+            }
+            else if (i == 1)
+            {
+                sentidoDesc = sentidos.FirstOrDefault(x => x.Key == (i + 1)).Value;
+
+                if (j == 1)
+                {
+                    if (k == 4)
+                        CreateCell(row, k, sentidoDesc, cellStyle);
+                    else if (k == 5)
+                        CreateCell(row, k, "", cellStyle);
+
+                    if (k == 9)
+                        CreateCell(row, k, sentidoDesc, cellStyle);
+                    else if (k >= 10 && k <= 11)
+                        CreateCell(row, k, "", cellStyle);
+                }
+            }
+        }
+
         public async Task<IActionResult> ExportFile()
         {
             string webRootFolder = _env.WebRootPath;
@@ -31,6 +187,12 @@ namespace SocietyAgendor.UI.Controllers
             string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, fileName);
             string local = Path.Combine(webRootFolder, fileName);
             FileInfo file = new FileInfo(local);
+
+            var sentidos = new Dictionary<int, string>
+            {
+                { 1, "Norte" },
+                { 2, "Sul" }
+            };
 
             var memoryStream = new MemoryStream();
 
@@ -51,16 +213,11 @@ namespace SocietyAgendor.UI.Controllers
 
                 #region Estilo célula 1
                 // borda
-                cellStyle.BorderTop = BorderStyle.Thick;
-                cellStyle.BorderRight = BorderStyle.Thick;
-                cellStyle.BorderBottom = BorderStyle.Thick;
-                cellStyle.BorderLeft = BorderStyle.Thick;
-
-                // cor borda
-                cellStyle.TopBorderColor = HSSFColor.Green.Index;
-                cellStyle.RightBorderColor = HSSFColor.Green.Index;
-                cellStyle.BottomBorderColor = HSSFColor.Green.Index;
-                cellStyle.LeftBorderColor = HSSFColor.Green.Index;
+                cellStyle.BorderTop = BorderStyle.Thin;
+                cellStyle.BorderRight = BorderStyle.Thin;
+                cellStyle.BorderBottom = BorderStyle.Thin;
+                cellStyle.BorderLeft = BorderStyle.Thin;
+                cellStyle.WrapText = true;
 
                 // alinhamento
                 cellStyle.Alignment = HorizontalAlignment.Center;
@@ -86,134 +243,96 @@ namespace SocietyAgendor.UI.Controllers
 
                 linha0.HeightInPoints = 26;
 
-                // DIA
-                var celula00HeaderDia = linha0.CreateCell(0);
-                var celula10HeaderDia = linha1.CreateCell(0);
-                var celula20HeaderDia = linha2.CreateCell(0);
+                IRow currentRow;
 
-                // HORA
-                var celula01HeaderHora = linha0.CreateCell(1);
-                var celula11HeaderHora = linha1.CreateCell(1);
-                var celula21HeaderHora = linha2.CreateCell(1);
+                // Percorro os sentidos
+                for (int i = 0; i < sentidos.Count; i++)
+                {
+                    // Percorre as linhas
+                    for (int j = 0; j < 3; j++)
+                    {
+                        currentRow = sheet.GetRow(j);
 
-                // CONTAGEM
-                var celula02HeaderContagem = linha0.CreateCell(2);
-                var celula03HeaderContagem = linha0.CreateCell(3);
-                var celula04HeaderContagem = linha0.CreateCell(4);
-                var celula05HeaderContagem = linha0.CreateCell(5);
+                        // Porcorre as células
+                        for (int k = 0; k < 12; k++)
+                        {
+                            CreateDiaCell(i, j, k, currentRow, cellStyle);
+                            CreateHoraCell(i, j, k, currentRow, cellStyle);
+                            CreateContagemCell(sentidos.Count, i, j, k, currentRow, cellStyle);
+                            CreateVelocidadeSentidoCell(sentidos.Count, i, j, k, currentRow, cellStyle);
+                            CreateSentidosCell(sentidos, i, j, k, currentRow, cellStyle);
+                        }
+                    }
+                }
 
-                // SENTIDO 1
-                var celula12HeaderSentido = linha1.CreateCell(2);
-                var celula13HeaderSentido = linha1.CreateCell(3);
-
-                // Auto - Sentido 1
-                var celula22HeaderSentido = linha2.CreateCell(2);
-
-                // Com - Sentido 1
-                var celula23HeaderSentido = linha2.CreateCell(3);
-
-                // SENTIDO 2
-                var celula14HeaderSentido = linha1.CreateCell(4);
-                var celula15HeaderSentido = linha1.CreateCell(5);
-
-                // Auto - Sentido 2
-                var celula24HeaderSentido = linha2.CreateCell(4);
-
-                // Com - Sentido 2
-                var celula25HeaderSentido = linha2.CreateCell(5);
-
-                celula00HeaderDia.CellStyle = cellStyle;
-                celula10HeaderDia.CellStyle = cellStyle;
-                celula20HeaderDia.CellStyle = cellStyle;
-                celula01HeaderHora.CellStyle = cellStyle;
-                celula11HeaderHora.CellStyle = cellStyle;
-                celula21HeaderHora.CellStyle = cellStyle;
-                celula02HeaderContagem.CellStyle = cellStyle;
-                celula03HeaderContagem.CellStyle = cellStyle;
-                celula04HeaderContagem.CellStyle = cellStyle;
-                celula05HeaderContagem.CellStyle = cellStyle;
-                celula12HeaderSentido.CellStyle = cellStyle;
-                celula13HeaderSentido.CellStyle = cellStyle;
-                celula14HeaderSentido.CellStyle = cellStyle;
-                celula15HeaderSentido.CellStyle = cellStyle;
-                celula22HeaderSentido.CellStyle = cellStyle;
-                celula23HeaderSentido.CellStyle = cellStyle;
-                celula24HeaderSentido.CellStyle = cellStyle;
-                celula25HeaderSentido.CellStyle = cellStyle;
-
-                celula00HeaderDia.SetCellValue("DIA");
-                celula00HeaderDia.SetCellType(CellType.String);
-
-                celula01HeaderHora.SetCellValue("HORA");
-                celula01HeaderHora.SetCellType(CellType.String);
-
-                celula02HeaderContagem.SetCellValue("CONTAGEM");
-                celula02HeaderContagem.SetCellType(CellType.String);
-
-                celula12HeaderSentido.SetCellValue("Leste");
-                celula12HeaderSentido.SetCellType(CellType.String);
-
-                celula14HeaderSentido.SetCellValue("Oeste");
-                celula14HeaderSentido.SetCellType(CellType.String);
-
-                celula22HeaderSentido.SetCellValue("AUTO");
-                celula22HeaderSentido.SetCellType(CellType.String);
-
-                celula23HeaderSentido.SetCellValue("COM");
-                celula23HeaderSentido.SetCellType(CellType.String);
-
-                celula24HeaderSentido.SetCellValue("AUTO");
-                celula24HeaderSentido.SetCellType(CellType.String);
-
-                celula25HeaderSentido.SetCellValue("COM");
-                celula25HeaderSentido.SetCellType(CellType.String);
-
-                // Mesclar células - A1:A3 - DIA
+                // Mesclagem de células
+                // A1:A3 - DIA
                 sheet.AddMergedRegion(new CellRangeAddress(0, 2, 0, 0));
 
-                // Mescla células - B1:B3 - HORA
+                // B1:B3 - HORA
                 sheet.AddMergedRegion(new CellRangeAddress(0, 2, 1, 1));
 
-                // Mescla células - C1:D1 - CONTAGEM
-                sheet.AddMergedRegion(new CellRangeAddress(0, 0, 2, 5));
-
-                // Mescla células - C2:D2 - SENTIDO 1
+                // C2:D2 - SENTIDO 1 - CONTAGEM
                 sheet.AddMergedRegion(new CellRangeAddress(1, 1, 2, 3));
 
-                // Mescla células - E2:F2 - SENTIDO 1
-                sheet.AddMergedRegion(new CellRangeAddress(1, 1, 4, 5));
+                if (sentidos.Count == 1)
+                {
+                    // C1:D1 - CONTAGEM
+                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, 2, 3));
+
+                    // E1:G1 - VELOCIDADE SENTIDO
+                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, 4, 6));
+
+                    // E2:G2 - SENTIDO 1 - VELOCIDADE SENTIDO
+                    sheet.AddMergedRegion(new CellRangeAddress(1, 1, 4, 6));
+                }
+                else
+                {
+                    // C1:F1 - CONTAGEM
+                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, 2, 5));
+
+                    // G1:L1 - VELOCIDADE SENTIDO
+                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, 6, 11));
+
+                    // E2:F2 - SENTIDO 2 - CONTAGEM
+                    sheet.AddMergedRegion(new CellRangeAddress(1, 1, 4, 5));
+
+                    // G2:I2 - SENTIDO 1 - VELOCIDADE SENTIDO
+                    sheet.AddMergedRegion(new CellRangeAddress(1, 1, 6, 8));
+
+                    // E2:F2 - SENTIDO 2 - VELOCIDADE SENTIDO
+                    sheet.AddMergedRegion(new CellRangeAddress(1, 1, 9, 11));
+                }
+
+                // Auto - Sentido 1
+                CreateCell(linha2, 2, "AUTO", cellStyle);
+
+                // Com - Sentido 1
+                CreateCell(linha2, 3, "COM", cellStyle);
+
+                // Auto - Sentido 2
+                CreateCell(linha2, 4, "AUTO", cellStyle);
+
+                // Com - Sentido 2
+                CreateCell(linha2, 5, "COM", cellStyle);
+
 
                 IRow rowLoop;
-                ICell cellLoop;
 
                 for (int i = 3; i < 10; i++)
                 {
                     rowLoop = sheet.CreateRow(i);
 
-                    for (int j = 0; j < 6; j++)
+                    for (int j = 0; j < 12; j++)
                     {
-                        cellLoop = rowLoop.CreateCell(j);
-                        cellLoop.CellStyle = cellStyle2;
-
                         if (j == 0)
-                        {
-                            cellLoop.SetCellValue(DateTime.Now.ToString("dd/MM/yyyy"));
-                            cellLoop.SetCellType(CellType.String);
-
-                        }
+                            CreateCell(rowLoop, j, DateTime.Now.ToString("dd/MM/yyyy"), cellStyle2);
                         else if (j == 1)
-                        {
-                            cellLoop.SetCellValue($"{DateTime.Now.ToString("HH:mm")} - {DateTime.Now.AddMinutes(i).ToString("HH:mm")}");
-                            cellLoop.SetCellType(CellType.String);
-                        }
+                            CreateCell(rowLoop, j, $"{DateTime.Now.ToString("HH:mm")} - {DateTime.Now.AddMinutes(i).ToString("HH:mm")}", cellStyle2);
                         else
-                        {
-                            cellLoop.SetCellValue(100 * j);
-                            cellLoop.SetCellType(CellType.Numeric);
-                        }
+                            CreateCell(rowLoop, j, 100 * j, cellStyle2);
                     }
                 }
-
 
                 workbook.Write(fs);
             }
